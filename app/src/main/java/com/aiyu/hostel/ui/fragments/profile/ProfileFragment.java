644c774475216deb8ab4c2f407c480ab13bf333c
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-04-24 22:38:08 +0530

    Implement login functionality with Google Sign-In and Firebase integration

--- app/src/main/java/com/aiyu/hostel/ui/fragments/profile/ProfileFragment.java
+++ app/src/main/java/com/aiyu/hostel/ui/fragments/profile/ProfileFragment.java
@@ -2,14 +2,19 @@ package com.aiyu.hostel.ui.fragments.profile;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.navigation.Navigation;
 
 import com.aiyu.hostel.R;
-import com.aiyu.hostel.databinding.FragmentDetailBinding;
+import com.aiyu.hostel.core.firebase.FirebaseInteraction;
 import com.aiyu.hostel.databinding.FragmentProfileBinding;
 import com.aiyu.hostel.utils.BaseFragment;
+import com.bumptech.glide.Glide;
+
+import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -20,10 +25,33 @@ public class ProfileFragment extends BaseFragment {
     }
 
     private FragmentProfileBinding binding;
+    @Inject
+    FirebaseInteraction firebaseInteraction;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         binding = FragmentProfileBinding.bind(view);
+
+        binding.logoutButton.setOnClickListener(v -> {
+            firebaseInteraction.logOut(unused -> {
+                var action = ProfileFragmentDirections.actionProfileFragmentToLogInFragment();
+                Navigation.findNavController(binding.getRoot()).navigate(action);
+            });
+        });
+        firebaseInteraction.getUser((user, exception) -> {
+            if (exception != null) {
+                Toast.makeText(requireContext(), exception.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (user != null) {
+                binding.relativeLayoutProfile.setVisibility(View.VISIBLE);
+                binding.linearLayoutLoading.setVisibility(View.GONE);
+                Glide.with(requireContext()).load(user.getProfilePic()).into(binding.imageViewProfile);
+                binding.profileName.setText(user.getName());
+                binding.profileEmail.setText(user.getEmail());
+            }
+        });
+
     }
 }
