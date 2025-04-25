0ae99571aac787175124e41f392982e228d97439
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-04-25 12:40:36 +0000

    Add hostel and ticket management features with CRUD operations

--- app/src/main/java/com/aiyu/hostel/core/firebase/FirebaseInteraction.java
+++ app/src/main/java/com/aiyu/hostel/core/firebase/FirebaseInteraction.java
@@ -1,10 +1,14 @@
 package com.aiyu.hostel.core.firebase;
 
 
+import com.aiyu.hostel.core.data.FoodItem;
+import com.aiyu.hostel.core.data.Hostel;
+import com.aiyu.hostel.core.data.Ticket;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -17,6 +21,11 @@ public class FirebaseInteraction {
 
     private static final String USER_DATABASE_PATH = "User";
 
+    private static final String HOSTEL_DATABASE_PATH = "Hostel";
+
+    private static final String TICKET_DATABASE_PATH = "Ticket";
+
+    private static final String FOOD_DATABASE_PATH = "Food";
 
 
     private final FirebaseFirestore firebaseFirestore;
@@ -69,6 +78,117 @@ public class FirebaseInteraction {
             onUserRetrieved.accept(null, e);
         });
     }
+
+    public void getHostel(BiConsumer<Hostel, Exception> onHostelRetrieved) {
+        if (firebaseAuth.getUid() == null) {
+            onHostelRetrieved.accept(null, new Exception("User not logged in"));
+            return;
+        }
+        firebaseFirestore.collection(HOSTEL_DATABASE_PATH).document(firebaseAuth.getUid()).get().addOnSuccessListener(documentSnapshot -> {
+            Hostel hostel = documentSnapshot.toObject(Hostel.class);
+            onHostelRetrieved.accept(hostel, null);
+        }).addOnFailureListener(e -> {
+            onHostelRetrieved.accept(null, e);
+        });
+    }
+
+    public void insertHostel(Hostel hostel, Consumer<Exception> onHostelAdded) {
+        var ref = firebaseFirestore.collection(HOSTEL_DATABASE_PATH);
+        String id = ref.document().getId();
+        hostel.setId(id);
+        ref.document(id).set(hostel).addOnSuccessListener(aVoid -> {
+            onHostelAdded.accept(null);
+        }).addOnFailureListener(onHostelAdded::accept);
+    }
+
+    public void updateHostel(Hostel hostel, Consumer<Exception> onHostelAdded) {
+        var ref = firebaseFirestore.collection(HOSTEL_DATABASE_PATH).document(hostel.getId());
+        ref.set(hostel).addOnSuccessListener(aVoid -> {
+            onHostelAdded.accept(null);
+        }).addOnFailureListener(onHostelAdded::accept);
+    }
+
+    public void deleteHostel(String hostelId, Consumer<Exception> onHostelDeleted) {
+        var ref = firebaseFirestore.collection(HOSTEL_DATABASE_PATH).document(hostelId);
+        ref.delete().addOnSuccessListener(aVoid -> {
+            onHostelDeleted.accept(null);
+        }).addOnFailureListener(onHostelDeleted::accept);
+    }
+
+
+    public void insertTicket(Ticket ticket, Consumer<Exception> onTicketAdded) {
+        var ref = firebaseFirestore.collection(TICKET_DATABASE_PATH);
+        String id = ref.document().getId();
+        ticket.setId(id);
+        ref.document(id).set(ticket).addOnSuccessListener(aVoid -> {
+            onTicketAdded.accept(null);
+        }).addOnFailureListener(onTicketAdded::accept);
+    }
+
+    public void updateTicket(Ticket ticket, Consumer<Exception> onTicketAdded) {
+        var ref = firebaseFirestore.collection(TICKET_DATABASE_PATH).document(ticket.getId());
+        ref.set(ticket).addOnSuccessListener(aVoid -> {
+            onTicketAdded.accept(null);
+        }).addOnFailureListener(onTicketAdded::accept);
+    }
+
+    public void deleteTicket(String ticketId, Consumer<Exception> onTicketDeleted) {
+        var ref = firebaseFirestore.collection(TICKET_DATABASE_PATH).document(ticketId);
+        ref.delete().addOnSuccessListener(aVoid -> {
+            onTicketDeleted.accept(null);
+        }).addOnFailureListener(onTicketDeleted::accept);
+    }
+
+    public void getTicket(BiConsumer<Ticket, Exception> onTicketRetrieved) {
+        if (firebaseAuth.getUid() == null) {
+            onTicketRetrieved.accept(null, new Exception("User not logged in"));
+            return;
+        }
+        firebaseFirestore.collection(TICKET_DATABASE_PATH).document(firebaseAuth.getUid()).get().addOnSuccessListener(documentSnapshot -> {
+            Ticket ticket = documentSnapshot.toObject(Ticket.class);
+            onTicketRetrieved.accept(ticket, null);
+        }).addOnFailureListener(e -> {
+            onTicketRetrieved.accept(null, e);
+        });
+    }
+
+
+    public void getAllFood(BiConsumer<List<FoodItem>, Exception> onFoodRetrieved) {
+        if (firebaseAuth.getUid() == null) {
+            onFoodRetrieved.accept(null, new Exception("User not logged in"));
+            return;
+        }
+        firebaseFirestore.collection(FOOD_DATABASE_PATH).get().addOnSuccessListener(queryDocumentSnapshots -> {
+            List<FoodItem> foodList = queryDocumentSnapshots.toObjects(FoodItem.class);
+            onFoodRetrieved.accept(foodList, null);
+        }).addOnFailureListener(e -> {
+            onFoodRetrieved.accept(null, e);
+        });
+    }
+
+    public void insertFood(FoodItem foodItem, Consumer<Exception> onFoodAdded) {
+        var ref = firebaseFirestore.collection(FOOD_DATABASE_PATH);
+        String id = ref.document().getId();
+        foodItem.setId(id);
+        ref.document(id).set(foodItem).addOnSuccessListener(aVoid -> {
+            onFoodAdded.accept(null);
+        }).addOnFailureListener(onFoodAdded::accept);
+    }
+
+    public void updateFood(FoodItem foodItem, Consumer<Exception> onFoodAdded) {
+        var ref = firebaseFirestore.collection(FOOD_DATABASE_PATH).document(foodItem.getId());
+        ref.set(foodItem).addOnSuccessListener(aVoid -> {
+            onFoodAdded.accept(null);
+        }).addOnFailureListener(onFoodAdded::accept);
+    }
+
+    public void deleteFood(String foodId, Consumer<Exception> onFoodDeleted) {
+        var ref = firebaseFirestore.collection(FOOD_DATABASE_PATH).document(foodId);
+        ref.delete().addOnSuccessListener(aVoid -> {
+            onFoodDeleted.accept(null);
+        }).addOnFailureListener(onFoodDeleted::accept);
+    }
+
 
 
     public void logOut(Consumer<Void> onLoggedOut) {
